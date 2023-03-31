b9b0a465a66e88360fdc275c67719ed4ba737489
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-03-31 13:44:06 +0200

    adding mongoose connect
    if applied, this commit will add a new mongoose connect uri

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
